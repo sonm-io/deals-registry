4ea80e4fd2305fe6579245d2094e9138c65000d1
Antony Shchukin <szwtdy@gmail.com>
2017-10-24 16:10:53 +0300

    external unused changes

--- contracts/Deals.sol
+++ contracts/Deals.sol
@@ -1,111 +1,306 @@
 pragma solidity ^0.4.14;
 
-//import "./IterableMapping.sol";
+
 import "./TSCToken.sol";
+
+
+contract iDeals {
+
+
+    // --------------------
+    // DEAL FUNCTION
+    // --------------------
+
+    // OpenDeal function to start new deal between hub and client
+    // create new deal and inserting it into indexes mappings
+    // returns bool, not more.
+    // other params returns via DealOpened event, spending by success result
+    function OpenDeal(address hub, address client, uint specificationHash, uint256 pricePerHour);
+
+    event DealOpened(address hub, address client, uint id);
+
+    // AcceptDeal function uses for assure deal with opponent
+    // in conclusion we decide, that deal must be started with client
+    // whatever opponent is hub.
+    // success case of execution AcceptDeal signaling that work must be started
+    // return bool, not more.
+    // other params returns via DealAccepted event, spending by success result of execution.
+    function AcceptDeal(uint id) returns (bool);
+
+    event DealAccepted(address hub, address client, uint id);
+
+    // CloseDeal function uses for abort work execution and put in order hub and client interact above this deal.
+    // May be used by hub - that might that hub want to end this deal otherwise:
+    // 1) hub found more paid and complex deal for our slot
+    // 2) client not paid deal more
+    // 3) other purpose about price, illegal use, not properly use of slot resourse
+    // Or may be used by client - that might that client want to stop work.
+    // если клиент завершает сделку - то хаб получает все заработанные деньги(через функцию PayDealOut() ),
+    // а клиент получает заблокированные токены обратно.
+    // CloseDeal signaling that work must be stopped, if was not done before.
+    // In this case
+    // return bool, not more.
+    // other params returns via DealClosed event, spending by success result of execution.
+    function CloseDeal() returns (bool);
+
+    event DealClosed(address hub, address client, uint id);
+
+    //    // PayDeal function - uses for deposit tokens for pay your deal
+    //    // any arrange of tokens must be allowed for contract for success execution of this function
+    //    // returns bool, not more.
+    //    // other params returns via DealPaid event, spending by success result of execution.
+    //    function PayDeal(uint id, uint256 amount) returns (bool);
+    //    event DealPaid(uint id, uint256 amount);
+    //
+    //    // PayOutDeal function uses for withdraw token from deal to hub
+    //    // PayDealOut withdraw tokens from deal to hub imedeatly by working time
+    //    // returns bool, not more.
+    //    // other params returns via DealPaidOut event, spending by success result of execution.
+    //    function PayOutDeal(uint id) returns (bool);
+    //    event DealPaidOut(uint id, uint256 amount);
+
+
+    // --------------------
+    // GETTERS
+    // --------------------
+
+    //    // GetHubByClient(address) returns (uint[])
+    //    // return array of uint responding all client deals without status
+    //    function GetDealsByClient(address client) returns (uint[]);
+    //
+    //    // GetDealsByHub(address) returns (uint[])
+    //    // return array of uint responding all hubs deals without status
+    //    function GetDealsByHub(address hub) returns (uint[]);
+    //
+    //    // GetDeal info by deal id
+    //    // returns any additional info about deal
+    //    function GetDealInfo(uint id) constant returns (uint specificicationHash, address client, address hub, uint256 pricePerHour, uint status);
+}
 
 
 contract Deals {
 
+    // --------------------
+    // MAIN STRUCTURES
+    // --------------------
+
+    struct Deal {
+    uint specificationHash;
+    address client;
+    address hub;
+    uint256 price;
+    uint256 securityAmount;
+    Status status;
+    uint startTime;
+    uint workTime;
+    uint endTime;
+    uint256 paidAmount;
+    }
+
+    // Status
+    // Now we decide to 3 avaiable status to deal
+    // Pending - after create, wait acepting - not working
+    // Accepted - time to work! At these status work not be doing(imidiatly after accepting, balance for deal is down, hub is down)
+    // Closed - deal is ended/or deal is canceled by client/hub by any reasons
+    enum Status{
+    Pending, // int(Status.Pending) = 0
+    Accepted, // int(Status.Accepted) = 1
+    Closed, // int(Status.Closed) = 2
+    ReportedFailFromHub,
+    ReportedFailFromClient
+    }
+
+    event DealOpened(address hub, address client, uint id);
+
+    event DealAccepted(address hub, address client, uint id);
+
+    event DealClosed(address hub, address client, uint id);
+
+    event DealCanceled(address hub, address client, uint id);
+
+    event WithdrawTokens(uint id, uint256 amount);
+
+    // For trusted execution above token work using SafeMath
     using SafeMath for uint256;
 
     uint dealSecuringPercentage = 10;
 
     TSCToken token;
 
-    enum Status{
-    Pending,
-    Accepted,
-    Closed
-    }
-
-    struct Deal {
-    uint256 specificationHash;
-    address client;
-    address hub;
-    uint256 price;
-    // todo: add time to this struct
-    Status status;
-    }
-
-    event DealOpened(address hub, address client, uint index);
-    event DealAccepted(address hub, address client, uint index);
-    event DealClosed(address hub, address client, uint index);
-
     uint dealAmount = 0;
 
     mapping (uint => Deal) deals;
 
     // TODO: must implement this
-    mapping (address => uint256) blockedBalance;
+    mapping (uint => uint256) blockedBalance;
+
+    mapping (uint => uint256) blockedSecurityBalance;
 
     // Client => []Hubs => []Deals
     mapping (address => uint[]) hubDealsIndex;
+
     mapping (address => uint[]) clientDealsIndex;
 
     function Deals(TSCToken _token){
         token = _token;
     }
 
+
+    // --------------------
+    // EXTERNAL
+    // --------------------
+
+
     // OpenDeal opening new deal
     // may to calling from everyone
-    function OpenDeal(address _hub, uint256 _specHash, uint256 _price){
-        // setting own parameters
-        address hubAddress = _hub;
-        address clientAddress = msg.sender;
-
-        // if client has no allowed tokens to contract must be thrown
-        require(IsPriceAllowed(clientAddress, _price));
-
-        require(token.transferFrom(clientAddress, this, _price));
-        blockedBalance[clientAddress] = blockedBalance[clientAddress].add(_price);
-
+    function OpenDeal(address _hub, uint _specificationHash, uint256 _price, uint256 _securityAmount, uint _workTime){
         // define index for new deal
+        var _client = msg.sender;
+
         uint dealIndex = dealAmount + 1;
         dealAmount = dealIndex;
 
         // create new deal
-        deals[dealIndex] = Deal(_specHash, clientAddress, hubAddress, _price, Status.Pending);
+        deals[dealIndex] = Deal(_specificationHash, _client, _hub, _price, _securityAmount, Status.Pending, 0, _workTime, 0, 0);
+
+        // if client has no allowed tokens to contract must be thrown
+        require(token.transferFrom(_client, this, _price));
+        blockedBalance[dealIndex] = blockedBalance[dealIndex].add(_price);
 
         // create index of this deal
-        // TODO: revert this
-        clientDealsIndex[clientAddress].push(dealIndex);
-        hubDealsIndex[hubAddress].push(dealIndex);
-//        IterableMapping.insert(dealsIndex[_client], uint144(bytes20(hubAddress) >> 16), dealIndex);
+        clientDealsIndex[_client].push(dealIndex);
+        hubDealsIndex[_hub].push(dealIndex);
 
-        DealOpened(hubAddress, clientAddress, dealIndex);
+        DealOpened(_hub, _client, dealIndex);
     }
 
-    function AcceptDeal(uint dealIndex) {
-        require(msg.sender == deals[dealIndex].hub);
+    function AcceptDeal(uint id) returns (bool){
 
-        // if hub has no allowed tokens to securingDeal must be thrown
-        require(IsPriceAllowed(deals[dealIndex].hub, (deals[dealIndex].price / dealSecuringPercentage )));
+        require(msg.sender == deals[id].hub);
 
-        deals[dealIndex].status = Status.Accepted;
+        deals[id].status = Status.Accepted;
+        deals[id].startTime = now;
 
-        DealAccepted(deals[dealIndex].hub, deals[dealIndex].client, dealIndex);
+
+        DealOpened(0x0000000000000000000000000000000000000000, 0x0000000000000000000000000000000000000000, id);
+
+        return false;
     }
 
-    function CloseDeal(uint dealIndex) payable {
-        var status = deals[dealIndex].status;
+    function CloseDeal(uint id) returns (bool){
+        DealClosed(0x0000000000000000000000000000000000000000, 0x0000000000000000000000000000000000000000, id);
 
-        // Its must be involve to revoke deal though CloseDeal
-        if (status == Status.Pending){
-            require(msg.sender == deals[dealIndex].hub);
-        }else if(status == Status.Accepted){
-            // TODO: must be spilt by time
-            require(token.transfer(deals[dealIndex].hub, deals[dealIndex].price));
-            blockedBalance[deals[dealIndex].client] = blockedBalance[deals[dealIndex].client].sub(deals[dealIndex].price);
+        return false;
+    }
+
+    function withdrawTokens(uint id){
+        // TODO: здесь должна быть проверка на статус сделки - если сделка Reported, то токены нельзя снять
+        var pricePerSecond = deals[id].price / deals[id].workTime;
+
+        var withdrawAmount = (now - deals[id].startTime) * pricePerSecond - deals[id].paidAmount;
+
+        require(token.transfer(deals[id].hub, withdrawAmount));
+        blockedBalance[id] = blockedBalance.sub(withdrawAmount);
+
+        WithdrawTokens(id, withdrawAmount);
+    }
+
+    // Must be used for canceling Deal from client
+    function CancelDeal(uint id){
+        require(msg.sender == deals[id].client);
+
+
+        if (deals[id].status == Status.Pending) {
+            require(token.transfer(deals[id].client, deals[id].price));
+            blockedBalance[id] = blockedBalance[id].sub(deals[id].price);
+        } else {
+            withdrawTokens(id);
+            require(token.transfer(deals[id].client, blockedBalance[id]));
+            blockedBalance[id] = blockedBalance[id].sub(deals[id].price);
+            // TODO: здесь должно быть возвращение токенов обеспечения хабу
         }
 
-        deals[dealIndex].status = Status.Closed;
 
-        DealClosed(deals[dealIndex].hub, deals[dealIndex].client, dealIndex);
+        deals[id].status = Status.Closed;
+
+        DealCanceled(deals[id].hub, deals[id].client, id);
     }
 
-    function GetDealInfo(uint dealIndex) constant returns (uint specHach, address client, address hub, uint price, uint status){
-        Deal storage deal = deals[dealIndex];
-        return (deal.specificationHash, deal.client, deal.hub, deal.price, uint(deal.status));
+    function FailWork(){
+        require(msg.sender == deals[id].hub);
+
+    }
+
+    function ReportFail(uint id){
+        require(deals[id].status == Status.Accepted);
+
+        if (msg.sender == deals[id].hub){
+            deals[id].status = Status.ReportedFailFromHub;
+            return;
+        }
+
+        if (msg.sender == deals[id].client){
+            deals[id].status = Status.ReportedFailFromClient;
+            return;
+        }
+
+        revert();
+    }
+
+    function ConfirmFail(){
+        if (deals[id].status == Status.ReportedFailFromClient){
+            withdrawToken
+        }
+
+        if (deals[id].status == Status.ReportedFailFromClient){
+
+        }
+
+        revert();
+    }
+
+    function RejectFail(){
+        require((deals[id].status == Status.ReportedFailFromHub)||(deals[id].status == Status.ReportedFailFromClient));
+
+    }
+
+    //    function AcceptDeal(uint dealIndex) {
+    //        require(msg.sender == deals[dealIndex].hub);
+    //
+    //        // if hub has no allowed tokens to securingDeal must be thrown
+    ////        require(IsPriceAllowed(deals[dealIndex].hub, (deals[dealIndex].price / dealSecuringPercentage )));
+    //
+    //        deals[dealIndex].status = Status.Accepted;
+    //        deals[dealIndex].status = Status.Accepted;
+    //
+    //        DealAccepted(deals[dealIndex].hub, deals[dealIndex].client, dealIndex);
+    //    }
+    //
+    //    function CloseDeal(uint id) payable {
+    //        var status = deals[id].status;
+    //
+    //        // Its must be involve to revoke deal though CloseDeal
+    ////        if (status == Status.Pending){
+    ////            require(msg.sender == deals[id].hub);
+    ////        }else if(status == Status.Accepted){
+    ////            // TODO: must be spilt by time
+    ////            require(token.transfer(deals[id].hub, deals[id].pricePerHour));
+    //////            blockedBalance[deals[id].client] = blockedBalance[deals[id].client].sub(deals[id].pricePerHour);
+    ////        }
+    //
+    //        deals[id].status = Status.Closed;
+    //
+    //        DealClosed(deals[id].hub, deals[id].client, id);
+    //    }
+    //
+
+    // --------------------
+    // GETTERS
+    // --------------------
+
+    function GetDealInfo(uint id) constant returns (uint specificicationHash, address client, address hub, uint256 price, uint status, uint startTime, uint workTime){
+        var deal = deals[id];
+        return (deal.specificationHash, deal.client, deal.hub, deal.price, uint(deal.status), deal.startTime, deal.workTime);
     }
 
     function GetDealByClient(address _client) constant returns (uint[]){
@@ -120,8 +315,8 @@ contract Deals {
         return dealAmount;
     }
 
-    function IsPriceAllowed(address addr, uint _price) returns (bool){
-        // this function malformed for first
-        return ((token.allowance(addr, this)) - _price)  >= 0;
+    function GetBlockedBalance(uint id) constant returns (uint256){
+        return blockedBalance[id];
     }
+
 }
--- test/deals.js
+++ test/deals.js
@@ -6,9 +6,12 @@ contract('Deals', async function (accounts) {
   var token
   var hub = accounts[1]
   var client = accounts[2]
+  var null_account = accounts[3]
 
   var test_spec = 2341234
   var test_price = 10000
+  var test_securityAmount = 1000
+  var test_workTime = 3600
 
   before(async function () {
     token = await TSC.deployed()
@@ -33,10 +36,12 @@ contract('Deals', async function (accounts) {
     assert.equal(clientBalance.toNumber(), 100000)
 
     let hubAllowance = await token.allowance(hub, deals.address)
-    console.log(hubAllowance)
+    assert.equal(100000, hubAllowance.toNumber())
   })
 
   it('test CreateDeal', async function () {
+    var clientBal = await token.balanceOf(client);
+
     var dealOpenEvent = deals.DealOpened()
     dealOpenEvent.watch(async function (err, result) {
       if (err) {
@@ -44,131 +49,167 @@ contract('Deals', async function (accounts) {
         return
       }
 
-      let deal_id = result.args.index
-      let deal_info = await deals.GetDealInfo.call(deal_id)
+      let contractBalance = await token.balanceOf(deals.address)
+      assert.equal(test_price, contractBalance.toNumber());
 
+      let deal_id = result.args.id
+      let deal_info = await deals.GetDealInfo.call(deal_id)
       assert.equal(deal_info[2], hub)
       assert.equal(deal_info[1], client)
       assert.equal(deal_info[0].toNumber(), test_spec)
       assert.equal(deal_info[3].toNumber(), test_price)
       assert.equal(deal_info[4].toNumber(), 0)
+      assert.equal(deal_info[5].toNumber(), 0)
+      assert.equal(deal_info[6].toNumber(), test_workTime)
+
+      let dealBalance = await deals.GetBlockedBalance.call(deal_id)
+      assert.equal(dealBalance.toNumber(), test_price)
+
+      let clientBalance = await token.balanceOf(client)
+      assert.equal(clientBalance.toNumber(), clientBal-test_price)
 
       dealOpenEvent.stopWatching()
     })
 
-    let deal_transaction = await deals.OpenDeal(hub, test_spec, test_price, { from: client })
+    let deal_transaction = await deals.OpenDeal(hub, test_spec, test_price, test_securityAmount, test_workTime, { from: client })
 
     let dealAmount = await deals.GetDealAmount.call()
     assert.equal(dealAmount, 1)
   })
 
-  it('test AcceptDeal', async function () {
-    var dealOpenEvent = deals.DealOpened()
-    var dealAcceptedEvent = deals.DealAccepted()
-    var deal_id
+  it('test CreateDeal -- fail', async function () {
 
-    dealOpenEvent.watch(async function (err, result) {
-      if (err) {
-        console.log(err)
-        return
-      }
+    let nullBalance = await token.balanceOf(null_account)
+    assert.equal(nullBalance.toNumber(), 0)
 
-      deal_id = result.args.index
+    try{
+      let deal_transaction = await deals.OpenDeal(hub, test_spec, test_price, test_securityAmount, test_workTime, { from: null_account })
+    }catch (e){
+      let contractBalance = await token.balanceOf(deals.address)
+      assert.equal(contractBalance.toNumber(), 10000)
 
-      let transaction = await deals.AcceptDeal(deal_id, { from: hub })
-      dealOpenEvent.stopWatching()
-    })
+      let dealAmount = await deals.GetDealAmount.call()
+      assert.equal(dealAmount, 1)
 
-    dealAcceptedEvent.watch(async function (err, result) {
-      if (err) {
-        console.log(err)
-        return
-      }
+      let dealBalance = await deals.GetBlockedBalance.call(2)
+      assert.equal(0, dealBalance.toNumber())
 
-      let deal_info = await deals.GetDealInfo.call(deal_id)
-
-      assert.equal(deal_info[2], hub)
-      assert.equal(deal_info[1], client)
-      assert.equal(deal_info[0].toNumber(), test_spec)
-      assert.equal(deal_info[3].toNumber(), test_price)
-      assert.equal(deal_info[4].toNumber(), 1)
-
-      dealAcceptedEvent.stopWatching()
-    })
-
-    let deal_transaction = await deals.OpenDeal(hub, test_spec, test_price, { from: client })
+      return
+    }
+    assert.Fail()
   })
 
-  it('test CloseDeal', async function () {
-    var dealOpenEvent = deals.DealOpened()
-    var dealAcceptedEvent = deals.DealAccepted()
-    var dealClosedEvent = deals.DealClosed()
-    var deal_id
 
-    dealOpenEvent.watch(async function (err, result) {
-      if (err) {
-        console.log(err)
-        return
-      }
-      deal_id = result.args.index
 
-      let transaction = await deals.AcceptDeal(deal_id, { from: hub })
-      dealOpenEvent.stopWatching()
-    })
-
-    dealAcceptedEvent.watch(async function (err, result) {
-      if (err) {
-        console.log(err)
-        return
-      }
-      let deal_info = await deals.GetDealInfo.call(deal_id)
-
-      assert.equal(deal_info[2], hub)
-      assert.equal(deal_info[1], client)
-      assert.equal(deal_info[0].toNumber(), test_spec)
-      assert.equal(deal_info[3].toNumber(), test_price)
-      assert.equal(deal_info[4].toNumber(), 1)
-
-      let transactions = await deals.CloseDeal(deal_id, { from: client })
-      dealAcceptedEvent.stopWatching()
-    })
-
-    dealClosedEvent.watch(async function (err, result) {
-      if (err) {
-        console.log(err)
-        return
-      }
-      let deal_info = await deals.GetDealInfo.call(deal_id)
-
-      assert.equal(deal_info[2], hub)
-      assert.equal(deal_info[1], client)
-      assert.equal(deal_info[0].toNumber(), test_spec)
-      assert.equal(deal_info[3].toNumber(), test_price)
-      assert.equal(deal_info[4].toNumber(), 2)
-
-      let hubBalance = await token.balanceOf.call(hub)
-      assert.equal(hubBalance.toNumber(), 110000)
-      let clientBalance = await token.balanceOf.call(client)
-      assert.equal(clientBalance.toNumber(), 70000)
-
-      dealClosedEvent.stopWatching()
-    })
-
-    let deal_transaction = await deals.OpenDeal(hub, test_spec, test_price, { from: client })
-  })
-
-  it('test GetDealAmount', async function () {
-    let dealAmount = await deals.GetDealAmount.call({from: accounts[0]})
-    assert.equal(dealAmount.toNumber(), 3)
-  })
+  // it('test AcceptDeal', async function () {
+  //   var dealOpenEvent = deals.DealOpened()
+  //   var dealAcceptedEvent = deals.DealAccepted()
+  //   var deal_id
+  //
+  //   dealOpenEvent.watch(async function (err, result) {
+  //     if (err) {
+  //       console.log(err)
+  //       return
+  //     }
+  //
+  //     deal_id = result.args.index
+  //
+  //     let transaction = await deals.AcceptDeal(deal_id, { from: hub })
+  //     dealOpenEvent.stopWatching()
+  //   })
+  //
+  //   dealAcceptedEvent.watch(async function (err, result) {
+  //     if (err) {
+  //       console.log(err)
+  //       return
+  //     }
+  //
+  //     let deal_info = await deals.GetDealInfo.call(deal_id)
+  //
+  //     assert.equal(deal_info[2], hub)
+  //     assert.equal(deal_info[1], client)
+  //     assert.equal(deal_info[0].toNumber(), test_spec)
+  //     assert.equal(deal_info[3].toNumber(), test_price)
+  //     assert.equal(deal_info[4].toNumber(), 1)
+  //
+  //     dealAcceptedEvent.stopWatching()
+  //   })
+  //
+  //   let deal_transaction = await deals.OpenDeal(hub, test_spec, test_price, { from: client })
+  // })
+  //
+  // it('test CloseDeal', async function () {
+  //   var dealOpenEvent = deals.DealOpened()
+  //   var dealAcceptedEvent = deals.DealAccepted()
+  //   var dealClosedEvent = deals.DealClosed()
+  //   var deal_id
+  //
+  //   dealOpenEvent.watch(async function (err, result) {
+  //     if (err) {
+  //       console.log(err)
+  //       return
+  //     }
+  //     deal_id = result.args.index
+  //
+  //     let transaction = await deals.AcceptDeal(deal_id, { from: hub })
+  //     dealOpenEvent.stopWatching()
+  //   })
+  //
+  //   dealAcceptedEvent.watch(async function (err, result) {
+  //     if (err) {
+  //       console.log(err)
+  //       return
+  //     }
+  //     let deal_info = await deals.GetDealInfo.call(deal_id)
+  //
+  //     assert.equal(deal_info[2], hub)
+  //     assert.equal(deal_info[1], client)
+  //     assert.equal(deal_info[0].toNumber(), test_spec)
+  //     assert.equal(deal_info[3].toNumber(), test_price)
+  //     assert.equal(deal_info[4].toNumber(), 1)
+  //
+  //     let transactions = await deals.CloseDeal(deal_id, { from: client })
+  //     dealAcceptedEvent.stopWatching()
+  //   })
+  //
+  //   dealClosedEvent.watch(async function (err, result) {
+  //     if (err) {
+  //       console.log(err)
+  //       return
+  //     }
+  //     let deal_info = await deals.GetDealInfo.call(deal_id)
+  //
+  //     assert.equal(deal_info[2], hub)
+  //     assert.equal(deal_info[1], client)
+  //     assert.equal(deal_info[0].toNumber(), test_spec)
+  //     assert.equal(deal_info[3].toNumber(), test_price)
+  //     assert.equal(deal_info[4].toNumber(), 2)
+  //
+  //     let hubBalance = await token.balanceOf.call(hub)
+  //     assert.equal(hubBalance.toNumber(), 110000)
+  //     let clientBalance = await token.balanceOf.call(client)
+  //     assert.equal(clientBalance.toNumber(), 70000)
+  //
+  //     dealClosedEvent.stopWatching()
+  //   })
+  //
+  //   let deal_transaction = await deals.OpenDeal(hub, test_spec, test_price, { from: client })
+  // })
+  //
+  // it('test GetDealAmount', async function () {
+  //   let dealAmount = await deals.GetDealAmount.call({from: accounts[0]})
+  //   assert.equal(dealAmount.toNumber(), 3)
+  // })
 
   it('test GetClientDeals', async function () {
     let clientDeals = await deals.GetDealByClient.call(client)
-    assert.equal(clientDeals.length, 3)
+    console.log('Client deals: ', clientDeals)
+    assert.equal(clientDeals.length, 1)
   })
 
   it('test GetClientDeals', async function () {
     let hubDeals = await deals.GetDealByHubAddress.call(hub)
-    assert.equal(hubDeals.length, 3)
+    console.log('Hub deals: ', hubDeals)
+    assert.equal(hubDeals.length, 1)
   })
 })

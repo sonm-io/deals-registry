f92863c9a850400f5eaaa9a29018dac739b15427
Antony Shchukin <szwtdy@gmail.com>
2017-11-23 22:22:26 +0300

    redeploy regenerate go-api

--- addresses.go
+++ addresses.go
@@ -1,4 +1,4 @@
 package tsc
 
-const TSCAddress string = "0x69a185fec67a9a67297064cc549d21a5aaca861f"
-const DealsAddress string = "0x311d1963b6896d9bc8c5834e5a22fa6ef4757bdd"
+const TSCAddress string = "0x9094e8ee00c76e29c11b974703dbfe8ab27a74ea"
+const DealsAddress string = "0x77028d6ce792a483d035ace642c3321168dda684"

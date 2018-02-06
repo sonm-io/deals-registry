f7898d3a40efaf6e0b75f7b63354d5edcdbdca41
quasisamurai <quazisamurai@gmail.com>
2018-02-06 13:37:27 +0300

    redeploy

--- addresses.go
+++ addresses.go
@@ -1,4 +1,4 @@
 package tsc
 
 const SNMTAddress string = "0x06bda3cf79946e8b32a0bb6a3daa174b577c55b5"
-const DealsAddress string = "0x430c4eaa00cb7e44ccc53eaf10b3f59b8ab2c79e"
+const DealsAddress string = "0x6dbc7e4bc031895cab4ef593fb431279ec9b7dcd"
